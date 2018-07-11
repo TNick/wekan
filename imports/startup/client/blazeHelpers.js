81405a022752a8c843fbfaeb482d4d6c68f66feb
Nicu Tofan <nicu.tofan@gmail.com>
2018-07-11 22:22:32 +0300

    Unsaved edits brought back

--- imports/startup/client/blazeHelpers.js
+++ imports/startup/client/blazeHelpers.js
@@ -1,8 +1,20 @@
 import { Blaze } from 'meteor/blaze';
+import { _ } from 'meteor/underscore';
 
 import { Filter } from '/imports/ui/components/utils/filter';
 import { Modal } from '/imports/ui/components/utils/modal';
-
+import { UnsavedEdits } from '/imports/ui/lib/unsavedEdits';
 
 Blaze.registerHelper('Filter', Filter);
 Blaze.registerHelper('Modal', Modal);
+Blaze.registerHelper('getUnsavedValue', (fieldName, docId, defaultTo) => {
+  // Workaround some blaze feature that pass a list of keywords arguments as the
+  // last parameter (even if the caller didn't specify any).
+  if (!_.isString(defaultTo)) {
+    defaultTo = '';
+  }
+  return UnsavedEdits.get({ fieldName, docId }, defaultTo);
+});
+Blaze.registerHelper('hasUnsavedValue', (fieldName, docId) => {
+  return UnsavedEdits.has({ fieldName, docId });
+});
